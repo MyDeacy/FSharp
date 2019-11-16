52b85e3608a12b715a0d1011b9f47e23a448f01f
Rin_ <mydeacy01010@gmail.com>
2019-11-16 14:43:32 +0900

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+type User() = 
+    class
+        [<DefaultValue>] val mutable name : String
+        [<DefaultValue>] val mutable age  : int
+
+        member this.Speak = printf "You >> ' I am %s %i years old! '" this.name this.age
+
+        member this.Name = this.name
+        member this.Age = this.age
+        member this.setName name = this.name <- name
+        member this.setAge age = this.age <- age
+    end;;
+let userObj = new User();;
+try
+    printf "What is your name\n ->"
+    let mutable line = stdin.ReadLine()
+    printf "Hello, %s\n" line
+    userObj.setName line
+    printf "How old are you？\n -> "
+    line <- stdin.ReadLine()
+    let age = int line
+    userObj.setAge age
+    userObj.Speak
+with
+    :? System.FormatException
+        -> printfn "You are not human"
+
